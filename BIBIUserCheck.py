ccffbe7ccfeff117df7fb256b067f01788c845d2
barry <360559261@qq.com>
2015-10-08 19:45:12 +0800

    20151008

--- BIBIUserCheck.py
+++ BIBIUserCheck.py
@@ -5,13 +5,14 @@ class BIBIUserCheck(object):
 	BIBI_REGISTER_BRACKETS = 3
 	BIBI_LOGIN_BRACKETS = 3
 	BIBI_SEARCH_BRACKETS = 1
-	
+	BIBI_FUZZY_BRACKETS = 2
 	'''
 		check if it is register request
 	'''
 	def isRigister(self, tcpData):
 		if (tcpData.find('register') == self.BIBI_INIT_POS):
 			if (tcpData.count('(') == self.BIBI_REGISTER_BRACKETS) and (tcpData.count(')') == self.BIBI_REGISTER_BRACKETS):
+				print tcpData, "Rigister"
 				return True
 		return False
 		
@@ -21,6 +22,7 @@ class BIBIUserCheck(object):
 	def isLogin(self, tcpData):
 		if (tcpData.find('login') == self.BIBI_INIT_POS):
 			if (tcpData.count('(') == self.BIBI_LOGIN_BRACKETS) and (tcpData.count(')') == self.BIBI_LOGIN_BRACKETS):
+				print tcpData, "Login"
 				return True
 		return False
 		
@@ -30,5 +32,20 @@ class BIBIUserCheck(object):
 	def isSearch(self, tcpData):
 		if (tcpData.find('search') == self.BIBI_INIT_POS):
 			if (tcpData.count('(') == self.BIBI_SEARCH_BRACKETS) and (tcpData.count(')') == self.BIBI_SEARCH_BRACKETS):
+				print tcpData, "Search"
 				return True
 		return False
+		
+	'''
+		check if it is fuzzy
+	'''
+	def isFuzzy(self, tcpData):
+		if (tcpData.find('fuzzy') == self.BIBI_INIT_POS):
+			if ((tcpData.count('(') == self.BIBI_SEARCH_BRACKETS) and (tcpData.count(')') == self.BIBI_SEARCH_BRACKETS))\
+			or ((tcpData.count('(') == self.BIBI_FUZZY_BRACKETS) and (tcpData.count(')') == self.BIBI_FUZZY_BRACKETS)):
+				print tcpData, "Fuzzy"
+				return True
+		return False
+	
+	
+	
